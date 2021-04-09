ed0efe679ccff5637a1e8ef0b280afb8d3340877
heatherww <pieda0123@yahoo.com.tw>
2021-04-10 03:03:55 +0800

    init spring boot

---
+++ src/main/java/com/example/currency_calculation/controller/calculationController.java
@@ -0,0 +1,14 @@
+package com.example.currency_calculation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class calculationController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/example/currency_calculation/CurrencyCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currency_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
